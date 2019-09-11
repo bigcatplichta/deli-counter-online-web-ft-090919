38bafa0e978a7dac243ffbb7c3ba08a096b3423b
Matt Plichta <bigcatplichta@gmail.com>
2019-09-11 18:47:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,6 +16,6 @@ end
 def take_a_number(list, name)
   list.push(name)
   
-  puts "Welcome #{name}. You are number #{list(name)} in line."
+  puts "Welcome #{name}. You are number #{list.index(name)} in line."
   binding.pry
 end
