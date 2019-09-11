4c922c2b8919d40a320399595dbc86f9b54160cd
Matt Plichta <bigcatplichta@gmail.com>
2019-09-11 18:51:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,4 +18,7 @@ def take_a_number(list, name)
   puts "Welcome, #{name}. You are number #{list.index(name) + 1} in line."
 end
 
-def now_serving
+def now_serving(list)
+  puts "Currently serving #{list.first}."
+  line.shift
+end
